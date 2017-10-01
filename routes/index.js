55a48c9e8bbe3e5d21c3ceb7cf26919f2b98184c
goldfido <goldfido@gmail.com>
2017-10-02 02:53:35 +0800

    fix bugs5

--- routes/index.js
+++ routes/index.js
@@ -33,8 +33,8 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new SpotifyStrategy({
         clientID: appKey,
         clientSecret: appSecret,
-        //callbackURL: 'http://spotify-iui.eu-4.evennode.com/callback'
-        callbackURL: 'http://localhost:3000/callback'
+        callbackURL: 'http://spotify-iui.eu-4.evennode.com/callback'
+        //callbackURL: 'http://localhost:3000/callback'
     },
         function (accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
