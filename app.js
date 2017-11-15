edaca8ce3e648f1331b731fca0361fa981174c18
goldfido <goldfido@gmail.com>
2017-11-15 21:26:18 +0800

    db

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var port         = process.env.port || 3000;
 var index = require('./routes/index');
 var app = express();
 
-mongoose.connect('mongodb://6938bf9f1157f79dea32a0e3caed7284:2010qhyjs@localhost:27017/spotify?authSource=admin', function (err) {
+mongoose.connect('mongodb://localhost:27017/spotify', function (err) {
     if (err) {
         console.log("connection error", err);
 
